da44c36a2df0a526008b7cf74ccd71148534de96
iluha23 <94638077+iluha23@users.noreply.github.com>
2022-07-02 11:27:43 +0300

    Создан задний фон
    создан класс заднего фона
    добавлены константы для цветов
    задний фон добавлен в игру

---
+++ land.py
@@ -0,0 +1,17 @@
+from snake_constants import LAND_COLOR, SCREEN_LENGTH, SCREEN_WIDTH
+from pygame import draw
+from pygame import Surface, Rect
+
+class Land:
+    """Задний фон игры"""
+
+    def __init__(self, surface: Surface) -> None:
+        """Базовый конструктор для создания фона игры"""
+        self.color = LAND_COLOR
+        self.length = SCREEN_LENGTH
+        self.width = SCREEN_WIDTH
+        self.surface = surface
+    
+    def draw(self) -> None:
+        """Прорисовка заднего фона"""
+        draw.rect(self.surface, self.color, Rect(0, 0, self.length, self.width))
--- main.py
+++ main.py
@@ -3,14 +3,18 @@ import pygame
 from pygame import display, event, key
 from pygame import QUIT, K_ESCAPE
 from snake_constants import *
+from land import Land
 
 screen = display.set_mode((SCREEN_LENGTH, SCREEN_WIDTH))  # Игровое окно
+land = Land(screen)  # Задний фон
 
 
 def game():
     """Игра запускается и работает до выключения пользователем"""
     play = True  # Работает ли игровое окно или нет
     while play:
+        land.draw()
+
         display.update()
 
         # Проверка, что игрок нажал на кнопку "закрыть"
