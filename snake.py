cd238b6c6779d9ca7d2365ab3d6919ed19d01d67
iluha23 <94638077+iluha23@users.noreply.github.com>
2022-07-10 14:33:40 +0300

    Создан блок, его размер и смена цвета земли
    Создана константа размера блока, обновлён цвет земли

--- snake.py
+++ snake.py
@@ -1,8 +1,9 @@
-from snake_constants import SNAKE_COLOR
+from snake_constants import SNAKE_COLOR, BLOCK_SIZE
 from pygame import Surface
 
 class Snake:
     def __init__(self, surface: Surface) -> None:
+        self.size = BLOCK_SIZE
         self.color = SNAKE_COLOR
         self.surface = surface
         self.coordinates_x = []
