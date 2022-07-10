26cf2cf806f1586d880aba7626485523e9f7d371
iluha23 <94638077+iluha23@users.noreply.github.com>
2022-07-10 14:48:41 +0300

    Изменения по принципу PEP
    Добавлена строка перед классом
    Изменен порядок импорта констант
    Добавлены пояснения, что будет содержаться в списка с координатами

--- snake.py
+++ snake.py
@@ -1,11 +1,12 @@
-from snake_constants import SNAKE_COLOR, BLOCK_SIZE
+from snake_constants import BLOCK_SIZE, SNAKE_COLOR
 from pygame import Surface
+
 
 class Snake:
     def __init__(self, surface: Surface) -> None:
         self.size = BLOCK_SIZE
         self.color = SNAKE_COLOR
         self.surface = surface
-        self.coordinates_x = []
-        self.coordinates_y = []
+        self.coordinates_x: list[int] = []
+        self.coordinates_y: list[int] = []
         self.speed = 10
