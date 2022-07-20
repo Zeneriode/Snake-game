d50a2367c5506fb5341938231f2b0460edd8e1b1
iluha23 <94638077+iluha23@users.noreply.github.com>
2022-07-20 14:18:51 +0300

    добавление нескольких функций, создание меняющихся координат
    Создание функций движения, поворота, поедания яблок, проверка врезания и прорисовки цвета змеи, теперь змейка будет появляться на разных координатах, созданы полоски на экране, для разделения на квадраты, обновление значений констант на более лучшие

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import pygame
-
-from pygame import display, event, key
-from pygame import QUIT, K_ESCAPE
-from snake_constants import *
 from land import Land
+from pygame import K_ESCAPE, QUIT, display, draw, event, key
+from snake_constants import SCREEN_LENGTH, SCREEN_WIDTH, BLOCK_SIZE
 
 screen = display.set_mode((SCREEN_LENGTH, SCREEN_WIDTH))  # Игровое окно
 land = Land(screen)  # Задний фон
@@ -15,13 +12,19 @@ def game():
     while play:
         land.draw()
 
+        for i in range(0, SCREEN_LENGTH, BLOCK_SIZE):
+            draw.line(screen, "black", (i, 0), (i, SCREEN_WIDTH))
+
+        for i in range(0, SCREEN_WIDTH, BLOCK_SIZE):
+            draw.line(screen, "black", (0, i), (SCREEN_LENGTH, i))
+
         display.update()
 
         # Проверка, что игрок нажал на кнопку "закрыть"
         for e in event.get():
             if e.type == QUIT:
                 play = False
-        
+
         # Проверка, что игрок нажал esc
         if key.get_pressed()[K_ESCAPE]:
             play = False
@@ -29,6 +32,3 @@ def game():
 
 if __name__ == "__main__":
     game()
-
-
-
