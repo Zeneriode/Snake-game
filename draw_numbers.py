ad022e27a578967b8d91319c1490c04fe216aa8c
iluha23 <94638077+iluha23@users.noreply.github.com>
2022-12-27 14:38:14 +0300

    счёт во время игры, рефакторинг кода, README файл
    реализуется счёт во время игры
    
    Настроена логика выбора четверти для прорисовки счета в игре
    
    Нормализованы полосы, которые писали счет в игре. Теперь счет понятен и виден всегда. Полосы, разделяющие экран на квадраты, рисуются под счетом, чтобы не заслонять его
    
    доделан счёт во время игры, написан README

--- draw_numbers.py
+++ draw_numbers.py
@@ -1,6 +1,8 @@
 """
 Реализация класса чисел
 """
+from random import choice
+
 from pygame import Rect, Surface, draw
 from snake_constants import BLOCK_SIZE, SCREEN_LENGTH, SCREEN_WIDTH, SNAKE_COLOR
 
@@ -11,9 +13,16 @@ class Number:
 
     def __init__(self, surface: Surface):
         """Базовый конструктор для создания счета очков"""
-        self.size = BLOCK_SIZE
+        self.size: float = BLOCK_SIZE + 1
         self.color = SNAKE_COLOR
         self.surface = surface
+        self.next_draw_in_quarter = choice(
+            [
+                self.__draw_in_quarter2,
+                self.__draw_in_quarter3,
+                self.__draw_in_quarter4,
+            ]
+        )
 
     def __number(self, blocks: list[tuple[float, float]]):
         for block in blocks:
@@ -24,121 +33,121 @@ class Number:
     def __number_1(self, center_x: float, center_y: float):
         """Создание пикселей первого числа"""
         blocks: list[tuple[float, float]] = [
-            (center_x - BLOCK_SIZE / 2, center_y - BLOCK_SIZE * 2),
-            (center_x - BLOCK_SIZE / 2, center_y - BLOCK_SIZE),
-            (center_x - BLOCK_SIZE / 2, center_y),
-            (center_x - BLOCK_SIZE / 2, center_y + BLOCK_SIZE),
-            (center_x - BLOCK_SIZE / 2, center_y + BLOCK_SIZE * 2),
-            (center_x - BLOCK_SIZE / 2 * 3, center_y - BLOCK_SIZE),
+            (center_x - self.size / 2, center_y - self.size * 2),
+            (center_x - self.size / 2, center_y - self.size),
+            (center_x - self.size / 2, center_y),
+            (center_x - self.size / 2, center_y + self.size),
+            (center_x - self.size / 2, center_y + self.size * 2),
+            (center_x - self.size / 2 * 3, center_y - self.size),
         ]
         self.__number(blocks)
 
     def __number_2(self, center_x: float, center_y: float):
         """Создание пикселей второго числа"""
         blocks: list[tuple[float, float]] = [
-            (center_x + BLOCK_SIZE / 2, center_y - BLOCK_SIZE * 2),
-            (center_x + BLOCK_SIZE / 2, center_y - BLOCK_SIZE),
-            (center_x + BLOCK_SIZE / 2, center_y),
-            (center_x + BLOCK_SIZE / 2, center_y + BLOCK_SIZE * 2),
-            (center_x - BLOCK_SIZE / 2, center_y - BLOCK_SIZE * 2),
-            (center_x - BLOCK_SIZE / 2, center_y),
-            (center_x - BLOCK_SIZE / 2, center_y + BLOCK_SIZE * 2),
-            (center_x - BLOCK_SIZE / 2 * 3, center_y - BLOCK_SIZE * 2),
-            (center_x - BLOCK_SIZE / 2 * 3, center_y),
-            (center_x - BLOCK_SIZE / 2 * 3, center_y + BLOCK_SIZE),
-            (center_x - BLOCK_SIZE / 2 * 3, center_y + BLOCK_SIZE * 2),
+            (center_x + self.size / 2, center_y - self.size * 2),
+            (center_x + self.size / 2, center_y - self.size),
+            (center_x + self.size / 2, center_y),
+            (center_x + self.size / 2, center_y + self.size * 2),
+            (center_x - self.size / 2, center_y - self.size * 2),
+            (center_x - self.size / 2, center_y),
+            (center_x - self.size / 2, center_y + self.size * 2),
+            (center_x - self.size / 2 * 3, center_y - self.size * 2),
+            (center_x - self.size / 2 * 3, center_y),
+            (center_x - self.size / 2 * 3, center_y + self.size),
+            (center_x - self.size / 2 * 3, center_y + self.size * 2),
         ]
         self.__number(blocks)
 
     def __number_3(self, center_x: float, center_y: float):
         """Создание пикселей третьего числа"""
         blocks: list[tuple[float, float]] = [
-            (center_x + BLOCK_SIZE / 2, center_y - BLOCK_SIZE * 2),
-            (center_x + BLOCK_SIZE / 2, center_y - BLOCK_SIZE),
-            (center_x + BLOCK_SIZE / 2, center_y),
-            (center_x + BLOCK_SIZE / 2, center_y + BLOCK_SIZE),
-            (center_x + BLOCK_SIZE / 2, center_y + BLOCK_SIZE * 2),
-            (center_x - BLOCK_SIZE / 2, center_y - BLOCK_SIZE * 2),
-            (center_x - BLOCK_SIZE / 2, center_y),
-            (center_x - BLOCK_SIZE / 2, center_y + BLOCK_SIZE * 2),
-            (center_x - BLOCK_SIZE / 2 * 3, center_y - BLOCK_SIZE * 2),
-            (center_x - BLOCK_SIZE / 2 * 3, center_y),
-            (center_x - BLOCK_SIZE / 2 * 3, center_y + BLOCK_SIZE * 2),
+            (center_x + self.size / 2, center_y - self.size * 2),
+            (center_x + self.size / 2, center_y - self.size),
+            (center_x + self.size / 2, center_y),
+            (center_x + self.size / 2, center_y + self.size),
+            (center_x + self.size / 2, center_y + self.size * 2),
+            (center_x - self.size / 2, center_y - self.size * 2),
+            (center_x - self.size / 2, center_y),
+            (center_x - self.size / 2, center_y + self.size * 2),
+            (center_x - self.size / 2 * 3, center_y - self.size * 2),
+            (center_x - self.size / 2 * 3, center_y),
+            (center_x - self.size / 2 * 3, center_y + self.size * 2),
         ]
         self.__number(blocks)
 
     def __number_4(self, center_x: float, center_y: float):
         """Создание пикселей четвёртого числа"""
         blocks: list[tuple[float, float]] = [
-            (center_x + BLOCK_SIZE / 2, center_y - BLOCK_SIZE * 2),
-            (center_x + BLOCK_SIZE / 2, center_y - BLOCK_SIZE),
-            (center_x + BLOCK_SIZE / 2, center_y),
-            (center_x + BLOCK_SIZE / 2, center_y + BLOCK_SIZE),
-            (center_x + BLOCK_SIZE / 2, center_y + BLOCK_SIZE * 2),
-            (center_x - BLOCK_SIZE / 2, center_y),
-            (center_x - BLOCK_SIZE / 2 * 3, center_y - BLOCK_SIZE * 2),
-            (center_x - BLOCK_SIZE / 2 * 3, center_y - BLOCK_SIZE),
-            (center_x - BLOCK_SIZE / 2 * 3, center_y),
+            (center_x + self.size / 2, center_y - self.size * 2),
+            (center_x + self.size / 2, center_y - self.size),
+            (center_x + self.size / 2, center_y),
+            (center_x + self.size / 2, center_y + self.size),
+            (center_x + self.size / 2, center_y + self.size * 2),
+            (center_x - self.size / 2, center_y),
+            (center_x - self.size / 2 * 3, center_y - self.size * 2),
+            (center_x - self.size / 2 * 3, center_y - self.size),
+            (center_x - self.size / 2 * 3, center_y),
         ]
         self.__number(blocks)
 
     def __number_5(self, center_x: float, center_y: float):
         """Создание пикселей пятого числа"""
         blocks: list[tuple[float, float]] = [
-            (center_x + BLOCK_SIZE / 2, center_y - BLOCK_SIZE * 2),
-            (center_x + BLOCK_SIZE / 2, center_y),
-            (center_x + BLOCK_SIZE / 2, center_y + BLOCK_SIZE),
-            (center_x + BLOCK_SIZE / 2, center_y + BLOCK_SIZE * 2),
-            (center_x - BLOCK_SIZE / 2, center_y - BLOCK_SIZE * 2),
-            (center_x - BLOCK_SIZE / 2, center_y),
-            (center_x - BLOCK_SIZE / 2, center_y + BLOCK_SIZE * 2),
-            (center_x - BLOCK_SIZE / 2 * 3, center_y - BLOCK_SIZE * 2),
-            (center_x - BLOCK_SIZE / 2 * 3, center_y - BLOCK_SIZE),
-            (center_x - BLOCK_SIZE / 2 * 3, center_y),
-            (center_x - BLOCK_SIZE / 2 * 3, center_y + BLOCK_SIZE * 2),
+            (center_x + self.size / 2, center_y - self.size * 2),
+            (center_x + self.size / 2, center_y),
+            (center_x + self.size / 2, center_y + self.size),
+            (center_x + self.size / 2, center_y + self.size * 2),
+            (center_x - self.size / 2, center_y - self.size * 2),
+            (center_x - self.size / 2, center_y),
+            (center_x - self.size / 2, center_y + self.size * 2),
+            (center_x - self.size / 2 * 3, center_y - self.size * 2),
+            (center_x - self.size / 2 * 3, center_y - self.size),
+            (center_x - self.size / 2 * 3, center_y),
+            (center_x - self.size / 2 * 3, center_y + self.size * 2),
         ]
         self.__number(blocks)
 
     def __number_6(self, center_x: float, center_y: float):
         """Создание пикселей шестого числа"""
         blocks: list[tuple[float, float]] = [
-            (center_x + BLOCK_SIZE / 2, center_y - BLOCK_SIZE * 2),
-            (center_x + BLOCK_SIZE / 2, center_y),
-            (center_x + BLOCK_SIZE / 2, center_y + BLOCK_SIZE),
-            (center_x + BLOCK_SIZE / 2, center_y + BLOCK_SIZE * 2),
-            (center_x - BLOCK_SIZE / 2, center_y - BLOCK_SIZE * 2),
-            (center_x - BLOCK_SIZE / 2, center_y),
-            (center_x - BLOCK_SIZE / 2, center_y + BLOCK_SIZE * 2),
-            (center_x - BLOCK_SIZE / 2 * 3, center_y - BLOCK_SIZE * 2),
-            (center_x - BLOCK_SIZE / 2 * 3, center_y - BLOCK_SIZE),
-            (center_x - BLOCK_SIZE / 2 * 3, center_y),
-            (center_x - BLOCK_SIZE / 2 * 3, center_y + BLOCK_SIZE),
-            (center_x - BLOCK_SIZE / 2 * 3, center_y + BLOCK_SIZE * 2),
+            (center_x + self.size / 2, center_y - self.size * 2),
+            (center_x + self.size / 2, center_y),
+            (center_x + self.size / 2, center_y + self.size),
+            (center_x + self.size / 2, center_y + self.size * 2),
+            (center_x - self.size / 2, center_y - self.size * 2),
+            (center_x - self.size / 2, center_y),
+            (center_x - self.size / 2, center_y + self.size * 2),
+            (center_x - self.size / 2 * 3, center_y - self.size * 2),
+            (center_x - self.size / 2 * 3, center_y - self.size),
+            (center_x - self.size / 2 * 3, center_y),
+            (center_x - self.size / 2 * 3, center_y + self.size),
+            (center_x - self.size / 2 * 3, center_y + self.size * 2),
         ]
         self.__number(blocks)
 
     def __number_7(self, center_x: float, center_y: float):
         """Создание пикселей седьмого числа"""
         blocks: list[tuple[float, float]] = [
-            (center_x + BLOCK_SIZE / 2, center_y - BLOCK_SIZE * 2),
-            (center_x + BLOCK_SIZE / 2, center_y - BLOCK_SIZE),
-            (center_x + BLOCK_SIZE / 2, center_y),
-            (center_x + BLOCK_SIZE / 2, center_y + BLOCK_SIZE),
-            (center_x + BLOCK_SIZE / 2, center_y + BLOCK_SIZE * 2),
-            (center_x - BLOCK_SIZE / 2, center_y - BLOCK_SIZE * 2),
-            (center_x - BLOCK_SIZE / 2 * 3, center_y - BLOCK_SIZE * 2),
-            (center_x - BLOCK_SIZE / 2 * 3, center_y - BLOCK_SIZE),
+            (center_x + self.size / 2, center_y - self.size * 2),
+            (center_x + self.size / 2, center_y - self.size),
+            (center_x + self.size / 2, center_y),
+            (center_x + self.size / 2, center_y + self.size),
+            (center_x + self.size / 2, center_y + self.size * 2),
+            (center_x - self.size / 2, center_y - self.size * 2),
+            (center_x - self.size / 2 * 3, center_y - self.size * 2),
+            (center_x - self.size / 2 * 3, center_y - self.size),
         ]
         self.__number(blocks)
 
     def __number_8(self, center_x: float, center_y: float):
         """Создание пикселей восьмого числа"""
         blocks: list[tuple[float, float]] = [
-            (center_x - BLOCK_SIZE / 2 * 3, center_y - BLOCK_SIZE * 2),
-            (center_x - BLOCK_SIZE / 2 * 3, center_y - BLOCK_SIZE),
-            (center_x - BLOCK_SIZE / 2 * 3, center_y),
-            (center_x - BLOCK_SIZE / 2 * 3, center_y + BLOCK_SIZE),
-            (center_x - BLOCK_SIZE / 2 * 3, center_y + BLOCK_SIZE * 2),
+            (center_x - self.size / 2 * 3, center_y - self.size * 2),
+            (center_x - self.size / 2 * 3, center_y - self.size),
+            (center_x - self.size / 2 * 3, center_y),
+            (center_x - self.size / 2 * 3, center_y + self.size),
+            (center_x - self.size / 2 * 3, center_y + self.size * 2),
         ]
         self.__number_3(center_x, center_y)
         self.__number(blocks)
@@ -146,7 +155,7 @@ class Number:
     def __number_9(self, center_x: float, center_y: float):
         """Создание пикселей девятого числа"""
         blocks: list[tuple[float, float]] = [
-            (center_x - BLOCK_SIZE / 2 * 3, center_y - BLOCK_SIZE),
+            (center_x - self.size / 2 * 3, center_y - self.size),
         ]
         self.__number_3(center_x, center_y)
         self.__number(blocks)
@@ -154,18 +163,18 @@ class Number:
     def __number_0(self, center_x: float, center_y: float):
         """Создание пикселей нулевого числа"""
         blocks: list[tuple[float, float]] = [
-            (center_x + BLOCK_SIZE / 2, center_y - BLOCK_SIZE * 2),
-            (center_x + BLOCK_SIZE / 2, center_y - BLOCK_SIZE),
-            (center_x + BLOCK_SIZE / 2, center_y),
-            (center_x + BLOCK_SIZE / 2, center_y + BLOCK_SIZE),
-            (center_x + BLOCK_SIZE / 2, center_y + BLOCK_SIZE * 2),
-            (center_x - BLOCK_SIZE / 2, center_y - BLOCK_SIZE * 2),
-            (center_x - BLOCK_SIZE / 2, center_y + BLOCK_SIZE * 2),
-            (center_x - BLOCK_SIZE / 2 * 3, center_y - BLOCK_SIZE * 2),
-            (center_x - BLOCK_SIZE / 2 * 3, center_y - BLOCK_SIZE),
-            (center_x - BLOCK_SIZE / 2 * 3, center_y),
-            (center_x - BLOCK_SIZE / 2 * 3, center_y + BLOCK_SIZE),
-            (center_x - BLOCK_SIZE / 2 * 3, center_y + BLOCK_SIZE * 2),
+            (center_x + self.size / 2, center_y - self.size * 2),
+            (center_x + self.size / 2, center_y - self.size),
+            (center_x + self.size / 2, center_y),
+            (center_x + self.size / 2, center_y + self.size),
+            (center_x + self.size / 2, center_y + self.size * 2),
+            (center_x - self.size / 2, center_y - self.size * 2),
+            (center_x - self.size / 2, center_y + self.size * 2),
+            (center_x - self.size / 2 * 3, center_y - self.size * 2),
+            (center_x - self.size / 2 * 3, center_y - self.size),
+            (center_x - self.size / 2 * 3, center_y),
+            (center_x - self.size / 2 * 3, center_y + self.size),
+            (center_x - self.size / 2 * 3, center_y + self.size * 2),
         ]
         self.__number(blocks)
 
@@ -194,25 +203,23 @@ class Number:
             case _:
                 return self.__number_9(center_x, center_y)
 
-    def __two_digits(self, points: int):
+    def __two_digits(self, points: int, center_x: float, center_y: float):
         """Прорисовка очков двумя цифрами"""
         first, second = points // 10, points % 10
-        center_x, center_y = SCREEN_LENGTH / 2, SCREEN_WIDTH / 2
 
-        distance_between_digits = BLOCK_SIZE * 10
+        distance_between_digits = self.size * 10
         first_x = center_x - distance_between_digits / 6
         second_x = center_x + distance_between_digits / 6
 
         self.__one_digit(first, first_x, center_y)
         self.__one_digit(second, second_x, center_y)
 
-    def __three_digits(self, points: int):
+    def __three_digits(self, points: int, center_x: float, center_y: float):
         """Прорисовка очков тремя цифрами"""
         first, second, third = points // 100, points // 10 % 10, points % 10
-        center_x, center_y = SCREEN_LENGTH / 2, SCREEN_WIDTH / 2
 
         # x - y - z
-        distance_between_digits = BLOCK_SIZE * 14
+        distance_between_digits = self.size * 14
         first_x = center_x - distance_between_digits / 2
         third_x = center_x + distance_between_digits / 2
 
@@ -220,11 +227,115 @@ class Number:
         self.__one_digit(second, center_x, center_y)
         self.__one_digit(third, third_x, center_y)
 
-    def draw(self, points: int):
+    def draw_in_total(self, points: int):
         """Прорисовка цифр"""
         if points // 100 > 0:
-            self.__three_digits(points)
+            self.__three_digits(points, SCREEN_LENGTH / 2, SCREEN_WIDTH / 2)
         elif points // 10 > 0:
-            self.__two_digits(points)
+            self.__two_digits(points, SCREEN_LENGTH / 2, SCREEN_WIDTH / 2)
         else:
             self.__one_digit(points, SCREEN_LENGTH / 2, SCREEN_WIDTH / 2)
+
+    def draw_in_game(self, points: int, snake_x: int, snake_y: int) -> None:
+        """Прорисовка цифр во время игры"""
+        reduce_coefficient = 2
+        self.size /= reduce_coefficient
+
+        if snake_x > SCREEN_LENGTH / 2 and snake_y < SCREEN_WIDTH / 2:
+            draw_in_quarter = self.next_draw_in_quarter
+        else:
+            draw_in_quarter = self.__draw_in_quarter1
+            self.next_draw_in_quarter = choice(
+                [
+                    self.__draw_in_quarter2,
+                    self.__draw_in_quarter3,
+                    self.__draw_in_quarter4,
+                ]
+            )
+        draw_in_quarter(points)
+
+        self.size *= reduce_coefficient
+
+    def __draw_in_quarter1(self, points: int):
+        """Выбор рисования в 1 четверти экрана"""
+        if points // 100 > 0:
+            self.__three_digits(
+                points,
+                SCREEN_LENGTH - 10 * self.size / 2,
+                SCREEN_WIDTH - 90 * self.size / 2,
+            )
+        elif points // 10 > 0:
+            self.__two_digits(
+                points,
+                SCREEN_LENGTH - 10 * self.size / 2,
+                SCREEN_WIDTH - 90 * self.size / 2,
+            )
+        else:
+            self.__one_digit(
+                points,
+                SCREEN_LENGTH - 10 * self.size / 2,
+                SCREEN_WIDTH - 90 * self.size / 2,
+            )
+
+    def __draw_in_quarter2(self, points: int):
+        """Выбор рисования во 2 четверти экрана"""
+        if points // 100 > 0:
+            self.__three_digits(
+                points,
+                SCREEN_LENGTH - 160 * self.size / 2,
+                SCREEN_WIDTH - 90 * self.size / 2,
+            )
+        elif points // 10 > 0:
+            self.__two_digits(
+                points,
+                SCREEN_LENGTH - 160 * self.size / 2,
+                SCREEN_WIDTH - 90 * self.size / 2,
+            )
+        else:
+            self.__one_digit(
+                points,
+                SCREEN_LENGTH - 160 * self.size / 2,
+                SCREEN_WIDTH - 90 * self.size / 2,
+            )
+
+    def __draw_in_quarter3(self, points: int):
+        """Выбор рисования в 3 четверти экрана"""
+        if points // 100 > 0:
+            self.__three_digits(
+                points,
+                SCREEN_LENGTH - 160 * self.size / 2,
+                SCREEN_WIDTH - 10 * self.size / 2,
+            )
+        elif points // 10 > 0:
+            self.__two_digits(
+                points,
+                SCREEN_LENGTH - 160 * self.size / 2,
+                SCREEN_WIDTH - 10 * self.size / 2,
+            )
+        else:
+            self.__one_digit(
+                points,
+                SCREEN_LENGTH - 160 * self.size / 2,
+                SCREEN_WIDTH - 10 * self.size / 2,
+            )
+
+    def __draw_in_quarter4(self, points: int):
+        """Выбор рисования в 4 четверти экрана"""
+        if points // 100 > 0:
+            self.__three_digits(
+                points,
+                SCREEN_LENGTH - 10 * self.size / 2,
+                SCREEN_WIDTH - 10 * self.size / 2,
+            )
+        elif points // 10 > 0:
+            self.__two_digits(
+                points,
+                SCREEN_LENGTH - 10 * self.size / 2,
+                SCREEN_WIDTH - 10 * self.size / 2,
+            )
+        else:
+            self.__one_digit(
+                points,
+                SCREEN_LENGTH - 10 * self.size / 2,
+                SCREEN_WIDTH - 10 * self.size / 2,
+            )
--- main.py
+++ main.py
@@ -1,10 +1,24 @@
+"""
+Основной файл игры. Используйте его для запуска самой игры.
+В этом файле создаются все предметы, необходимые для игры, а также реализованы
+функции, которые запускают игру и экран после игры.
+"""
+from time import sleep
+
+from draw_numbers import Number
 from food import Food
 from land import Land
-from time import sleep
-from pygame import K_ESCAPE, QUIT, display, draw, event, key, time
+
+# pylint: disable=no-name-in-module
+from pygame import K_ESCAPE, QUIT, display, draw, event, font, init, key, time
 from snake import Snake
-from draw_numbers import Number
-from snake_constants import BLOCK_SIZE, SCREEN_LENGTH, SCREEN_WIDTH
+from snake_constants import (
+    BLOCK_SIZE,
+    LINE_COLOR,
+    SCREEN_LENGTH,
+    SCREEN_WIDTH,
+    SNAKE_COLOR,
+)
 
 screen = display.set_mode((SCREEN_LENGTH, SCREEN_WIDTH))  # Игровое окно
 land = Land(screen)  # Задний фон
@@ -12,20 +26,25 @@ snake = Snake(screen)  # Змея
 food = Food(screen)  # еда
 clock = time.Clock()  # смена кадров
 result = Number(screen)
-reason_of_death = None
+# pylint: disable=C0103
+reason_of_death = ""
 
 
-def check_food():
+def check_food() -> Food:
     """Проверяет, надо ли есть еду"""
-    global food
-    if snake.coordinates_x[0] == food.x_coordinate and snake.coordinates_y[0] == food.y_coordinate:
-        food = Food(screen)
+    if (
+        snake.coordinates_x[0] == food.x_coordinate
+        and snake.coordinates_y[0] == food.y_coordinate
+    ):
         snake.grow()
+        return Food(screen)
+    return food
 
 
 def game():
     """Игра запускается и работает до выключения пользователем"""
-    global reason_of_death
+    # pylint: disable=[C0103, W0603]
+    global reason_of_death, food
     play = True  # Работает ли игровое окно или нет
     frame = 0
     while play:
@@ -34,15 +53,18 @@ def game():
             snake.draw()
             food.draw()
             snake.move()
-            check_food()
+            food = check_food()
 
         snake.change_direction()
 
         for i in range(0, SCREEN_LENGTH, BLOCK_SIZE):
-            draw.line(screen, "black", (i, 0), (i, SCREEN_WIDTH))
+            draw.line(screen, LINE_COLOR, (i, 0), (i, SCREEN_WIDTH))
 
         for i in range(0, SCREEN_WIDTH, BLOCK_SIZE):
-            draw.line(screen, "black", (0, i), (SCREEN_LENGTH, i))
+            draw.line(screen, LINE_COLOR, (0, i), (SCREEN_LENGTH, i))
+
+        result.draw_in_game(len(snake), snake.coordinates_x[0],
+                            snake.coordinates_y[0])
 
         display.update()
         clock.tick(200)
@@ -54,23 +76,33 @@ def game():
         for e_check in event.get():
             if e_check.type == QUIT:
                 play = False
+                reason_of_death = "Вы покинули игру!"
 
         # Проверка, что игрок нажал esc
         if key.get_pressed()[K_ESCAPE]:
             play = False
+            reason_of_death = "Вы покинули игру!"
 
 
 def game_over():
     """Конец игры"""
-    # причины смерти:
-    # 1) столкновение со стенкой
-    # 2) выход из игры
     land.draw()
-    result.draw(len(snake))
+    result.draw_in_total(len(snake))
+    # Задаём шрифт и размер текста
+    text_font = font.Font("C:\\WINDOWS\\Fonts\\candaral.ttf", 50)
+    # Задаём контент и цвет текста
+    text = text_font.render(reason_of_death, False, SNAKE_COLOR)
+    # Задаём форму и местонахождение текста
+    text_line = text.get_rect()
+    text_line.center = (SCREEN_LENGTH / 2, SCREEN_WIDTH / 2 + 200)
+    # рисуем текст
+    screen.blit(text, text_line)
+
     display.update()
-    sleep(1.5)
+    sleep(2.5)
 
 
+init()
 if __name__ == "__main__":
     game()
     game_over()
